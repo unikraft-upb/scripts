e511b77ba4996b5ce06dc465a0dcecfcd755ae80
Razvan Deaconescu <razvand@unikraft.io>
2023-05-27 12:15:38 +0300

    make-based/app-elfloader: Use default branches in do.sh
    Custom branches (`skuenzer`, `csvancea`) were used for remotes with
    not-yet-upstreamed features. This is no longer the case, as all branches
    are now in.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- make-based/app-elfloader/do.sh
+++ make-based/app-elfloader/do.sh
@@ -27,20 +27,6 @@ setup_elfloader()
     git reset --hard HEAD
     git checkout staging > /dev/null 2>&1
 
-    git remote rm csvancea > /dev/null 2>&1
-    git remote add csvancea https://github.com/csvancea/app-elfloader/
-    git fetch csvancea
-    git checkout staging
-    git branch -D csvancea/fix/argv-parser > /dev/null 2>&1
-    git branch -D csvancea/fix/unaligned-stack > /dev/null 2>&1
-    git branch csvancea/fix/argv-parser csvancea/csvancea/fix/argv-parser
-    git branch csvancea/fix/unaligned-stack csvancea/csvancea/fix/unaligned-stack
-
-    git branch -D setup > /dev/null 2>&1
-    git checkout -b setup staging
-    git rebase csvancea/fix/argv-parser
-    git rebase csvancea/fix/unaligned-stack
-
     if test ! -d ../run-app-elfloader; then
         git clone https://github.com/unikraft/run-app-elfloader ../run-app-elfloader
     fi
@@ -51,75 +37,6 @@ setup_elfloader()
     if test ! -d ../dynamic-apps; then
         git clone https://github.com/unikraft/dynamic-apps ../dynamic-apps
     fi
-}
-
-setup_lwip()
-{
-    git reset --hard HEAD
-    git checkout staging > /dev/null 2>&1
-
-    git remote rm marcrittinghaus > /dev/null 2>&1
-    git remote add marcrittinghaus https://github.com/marcrittinghaus/lib-lwip
-    git fetch marcrittinghaus
-    git branch -D mritting/pr_sock_nonblock > /dev/null 2>&1
-    git branch mritting/pr_sock_nonblock marcrittinghaus/mritting/pr_sock_nonblock
-
-    git branch -D setup > /dev/null 2>&1
-    git checkout -b setup staging
-    git rebase mritting/pr_sock_nonblock
-}
-
-setup_unikraft()
-{
-    git reset --hard HEAD
-    git checkout staging > /dev/null 2>&1
-
-    git remote rm csvancea > /dev/null 2>&1
-    git remote add csvancea https://github.com/csvancea/unikraft
-    git fetch csvancea
-    git branch -D csvancea/fix/stat-mode-type > /dev/null 2>&1
-    git branch csvancea/fix/stat-mode-type csvancea/csvancea/fix/stat-mode-type
-    git branch -D csvancea/feature/allow-non-writable-shared-file-mappings > /dev/null 2>&1
-    git branch csvancea/feature/allow-non-writable-shared-file-mappings csvancea/csvancea/feature/allow-non-writable-shared-file-mappings
-
-    git remote rm andraprs > /dev/null 2>&1
-    git remote add andraprs https://github.com/andraprs/unikraft
-    git fetch andraprs
-    git branch -D 9pfs-symlink-fix > /dev/null 2>&1
-    git branch 9pfs-symlink-fix andraprs/9pfs-symlink-fix
-
-    git remote rm marcrittinghaus > /dev/null 2>&1
-    git remote add marcrittinghaus https://github.com/marcrittinghaus/unikraft
-    git fetch marcrittinghaus
-    git branch -D mritting/pr_appcompat_fixes > /dev/null 2>&1
-    git branch mritting/pr_appcompat_fixes marcrittinghaus/mritting/pr_appcompat_fixes
-    git branch -D mritting/pr_prsyscall_struct > /dev/null 2>&1
-    git branch mritting/pr_prsyscall_struct marcrittinghaus/mritting/pr_prsyscall_struct
-    git branch -D mritting/pr_fix_accept_nonblock > /dev/null 2>&1
-    git branch mritting/pr_fix_accept_nonblock marcrittinghaus/mritting/pr_fix_accept_nonblock
-    git branch -D mritting/pr_fix_devfs > /dev/null 2>&1
-    git branch mritting/pr_fix_devfs marcrittinghaus/mritting/pr_fix_devfs
-
-    git remote rm unikraft-upb > /dev/null 2>&1
-    git remote add unikraft-upb https://github.com/unikraft-upb/unikraft
-    git fetch unikraft-upb
-    git branch -D StefanJum/fix-symlink-error > /dev/null 2>&1
-    git branch StefanJum/fix-symlink-error unikraft-upb/StefanJum/fix-symlink-error
-
-    git remote rm John-Ted > /dev/null 2>&1
-    git remote add John-Ted https://github.com/John-Ted/unikraft
-    git fetch John-Ted
-    git branch -D remove_clone3 > /dev/null 2>&1
-    git branch remove_clone3 John-Ted/remove_clone3
-
-    git branch -D setup > /dev/null 2>&1
-    git checkout -b setup staging
-    git rebase 9pfs-symlink-fix
-    git rebase csvancea/fix/stat-mode-type
-    git rebase csvancea/feature/allow-non-writable-shared-file-mappings
-    git rebase mritting/pr_appcompat_fixes
-    git rebase mritting/pr_prsyscall_struct
-    git rebase StefanJum/fix-symlink-error
 }
 
 setup_app()
